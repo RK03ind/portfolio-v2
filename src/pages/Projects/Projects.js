c6396819d1f9b8411992b239d5b7c2f89e4e15ad
RK03ind <gamitech95@gmail.com>
2022-09-03 16:43:43 +0530

    Add: New projects to neogProjectsData

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -3,7 +3,7 @@ import ProjectItem from "./components/ProjectItem";
 import "./Projects.css";
 
 const Projects = () => {
-  const projectData = [
+  const projectsData = [
     {
       title: "Expense Tracker (PWA)",
       desc: "A very simple Expense Tracker PWA with intiutive expense tracking charts and complete offline working capabilities. Built with ReactJS and Workbox.",
@@ -39,14 +39,80 @@ const Projects = () => {
     },
   ];
 
-  const neogProjectData = [
+  const neogProjectsData = [
     {
-      title: "Expense Tracker (PWA)",
-      desc: "A very simple Expense Tracker PWA with intiutive expense tracking charts and complete offline working capabilities. Built with ReactJS and Workbox.",
-      timeTaken: "Dec 2021",
-      techStack: ["react", "html", "css", "javascript"],
-      liveLink: "https://expense-tracker-offline.netlify.app/",
-      githubLink: "https://github.com/RK03ind/expense-tracker-pwa",
+      title: "Minion Translator (Mark 6)",
+      desc: "Translates standard english to minion language",
+      timeTaken: "September 2022",
+      techStack: ["html", "css", "javascript"],
+      liveLink: "https://minion-translator-rk03.netlify.app/",
+      githubLink:
+        "https://github.com/RK03ind/Minion-Translator-Neog---Mark-Six-/",
+    },
+    {
+      title: "Shakespeare Translator (Mark 7)",
+      desc: "Translates standard english to shakespearean english",
+      timeTaken: "September 2022",
+      techStack: ["html", "css", "javascript"],
+      liveLink: "https://english-shakespeare-translator.netlify.app/",
+      githubLink:
+        "https://github.com/RK03ind/Shakespeare-Translator--MarkSeven/",
+    },
+    {
+      title: "Emoji Interpreter (Mark 8)",
+      desc: "Interprets the meaning of certain emojis",
+      timeTaken: "September 2022",
+      techStack: ["html", "css", "javascript", "react"],
+      liveLink: "https://emoji-meaning-rk03.netlify.app/",
+      githubLink: "https://github.com/RK03ind/emoji-interpreter--mark8",
+    },
+    {
+      title: "Good Games (Mark 9)",
+      desc: "Provides game recommendations from various genres.",
+      timeTaken: "September 2022",
+      techStack: ["html", "css", "javascript", "react"],
+      liveLink: "https://goodgames-rk03.netlify.app/",
+      githubLink: "https://github.com/RK03ind/good-games--mark9",
+    },
+    {
+      title: "Cash Register Manager (Mark 10)",
+      desc: "Calculates the amount of change the cashier must return.",
+      timeTaken: "September 2022",
+      techStack: ["html", "css", "javascript"],
+      liveLink: "https://cash-register-manager-rk03.netlify.app/",
+      githubLink: "https://github.com/RK03ind/Cash-Register-Manager--mark10",
+    },
+    {
+      title: "Is Your Birthday Lucky (Mark 11)",
+      desc: "Checks if your birthdate is lucky",
+      timeTaken: "September 2022",
+      techStack: ["html", "css", "javascript"],
+      liveLink: "https://lucky-birthday-rk03.netlify.app/",
+      githubLink: "https://github.com/RK03ind/Is-Your-Birthday-Lucky--mark11",
+    },
+    {
+      title: "Fun with Triangles  (Mark 12)",
+      desc: "Webapp for quiz and calculator related to triangle",
+      timeTaken: "September 2022",
+      techStack: ["html", "css", "javascript", "react"],
+      liveLink: "https://fun-with-triangles-rk03.netlify.app/",
+      githubLink: "https://github.com/RK03ind/fun-with-triangles--mark12",
+    },
+    {
+      title: "Palindrome Birthday (Mark 13)",
+      desc: "Checks if your birthdate is palidrome",
+      timeTaken: "September 2022",
+      techStack: ["html", "css", "javascript"],
+      liveLink: "https://palindrome-dob-rk03.netlify.app/",
+      githubLink: "https://github.com/RK03ind/palidrome-birthday--mark13",
+    },
+    {
+      title: "Profit or Loss (Mark 14)",
+      desc: "Determines stock's profit or loss",
+      timeTaken: "September 2022",
+      techStack: ["html", "css", "javascript"],
+      liveLink: "https://stock-rk03.netlify.app/",
+      githubLink: "https://github.com/RK03ind/Profit-Loss-Calculator--mark14",
     },
   ];
 
@@ -73,7 +139,7 @@ const Projects = () => {
       </div>
       <div className="project-item-container">
         {projectState
-          ? projectData.map((project, index) => {
+          ? projectsData.map((project, index) => {
               return (
                 <ProjectItem
                   {...project}
@@ -82,7 +148,7 @@ const Projects = () => {
                 />
               );
             })
-          : neogProjectData.map((project, index) => {
+          : neogProjectsData.map((project, index) => {
               return (
                 <ProjectItem
                   {...project}
