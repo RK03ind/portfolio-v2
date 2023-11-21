c3026cfeb2f98006f32c2da7ddd9ca74cd485cec
RK03ind <gamitech95@gmail.com>
2023-11-21 20:12:02 +0530

    UPDATED: Projects Section

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -6,18 +6,26 @@ import "./Projects.css";
 
 const Projects = () => {
   const projectsData = [
+    // {
+    //   title: "Expense Tracker (PWA)",
+    //   desc: "A very simple Expense Tracker PWA with intiutive expense tracking charts and complete offline working capabilities. Built with ReactJS and Workbox.",
+    //   timeTaken: "Dec 2021",
+    //   techStack: ["react", "html", "css", "javascript"],
+    //   liveLink: "https://expense-tracker-offline.netlify.app/",
+    //   githubLink: "https://github.com/RK03ind/expense-tracker-pwa",
+    // },
     {
-      title: "Expense Tracker (PWA)",
-      desc: "A very simple Expense Tracker PWA with intiutive expense tracking charts and complete offline working capabilities. Built with ReactJS and Workbox.",
-      timeTaken: "Dec 2021",
-      techStack: ["react", "html", "css", "javascript"],
-      liveLink: "https://expense-tracker-offline.netlify.app/",
-      githubLink: "https://github.com/RK03ind/expense-tracker-pwa",
+      title: "Movie List",
+      desc: "A MERN Stack based Web App for viewing & searching trending/old movies and making watchlists with them.",
+      timeTaken: "Feb 2023 - March 2023",
+      techStack: ["react", "express", "mongo", "node"],
+      liveLink: "https://movie-list.rudrakonar.dev/",
+      githubLink: "https://github.com/RK03ind/movie-list-public",
     },
     {
       title: "Realtime Chat App",
       desc: "A Realtime Chat WebApp built with ReactJS, Firebase and MUI Library including User Authentication, complete CRUD operations and multiple features.",
-      timeTaken: "Dec 2021 - Jan 2021",
+      timeTaken: "Dec 2023 - Jan 2023",
       techStack: ["react", "html", "css", "firebase"],
       liveLink: "https://livechat1.netlify.app/",
       githubLink: "https://github.com/RK03ind/Realtime-Chat",
@@ -25,9 +33,9 @@ const Projects = () => {
     {
       title: "Travellers Stash",
       desc: "A MERN Stack based Social Media App. A place to share the beauty of this amazing world with people. Share places you've visited or want to visit, share any knowledge or facts regarding that place.",
-      timeTaken: "Feb 2022 - March 2022",
+      timeTaken: "Feb 2023 - March 2023",
       techStack: ["react", "express", "mongo", "node"],
-      liveLink: "https://travellers-stash.netlify.app/",
+      liveLink: "https://travellers-stash.rudrakonar.dev/",
       githubLink: "https://github.com/RK03ind/Travellers-Stash-Public",
     },
     {
